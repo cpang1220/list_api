ad89a03eb50159152c45695b86e046bc6618598c
cpang1220 <62884385+cpang1220@users.noreply.github.com>
2020-11-01 15:38:17 +0000

    Update list_sum file

--- app/list_sum.py
+++ app/list_sum.py
@@ -5,11 +5,12 @@ from flask import json
 
 
 def get_list_sum():
+    const.HTTP_ERROR = False
     input_list_response = requests.get(const.LOCALHOST_API_URL + '/input_list')
     try:
         input_list_response.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        const.HTTP_ERROR = False
+        const.HTTP_ERROR = True
         status_code = e.response.status_code
         message = e.response.reason
         return error_obj(message,status_code)
